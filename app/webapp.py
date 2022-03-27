04feea2b5b246124a39dc10b641ad5a875aa0155
barbarisch <bpeterson926@gmail.com>
2022-03-27 13:54:56 -0700

    basic path to destination function in webapp.

--- app/webapp.py
+++ app/webapp.py
@@ -5,12 +5,12 @@ from dbmodel import *
 
 
 def make_node(node_id, node_name, node_type):
-    print('Adding node', node_id, node_name)
+    # print('Adding node', node_id, node_name)
     return {'id': node_id, 'name': node_name, 'type': node_type}
 
 
 def make_edge(edge_id, edge_from, edge_to, edge_label):
-    print('Adding edge', edge_from, edge_to, edge_label)
+    # print('Adding edge', edge_from, edge_to, edge_label)
     return {'id': edge_id, 'from': edge_from, 'to': edge_to, 'label': edge_label}
 
 
@@ -20,17 +20,44 @@ class BloodRoostrWebApp:
         self.db_session = get_session(self.db_conn)
         self.graph = nx.DiGraph()
 
+    def name_to_sid(self, name):
+        res = self.db_session.query(Group).filter_by(name=name.upper()).first()
+        if res:
+            return res.objectSid
+        res = self.db_session.query(ADUser).filter_by(name=name.upper()).first()
+        if res:
+            return res.objectSid
+        res = self.db_session.query(Machine).filter_by(name=name.upper()).first()
+        if res:
+            return res.objectSid
+        res = self.db_session.query(GPO).filter_by(name=name.upper()).first()
+        if res:
+            return res.objectGUID
+        res = self.db_session.query(ADOU).filter_by(name=name.upper()).first()
+        if res:
+            return res.objectGUID
+        return ''
+
     def graph_update(self, json_data):
+        print('HERE!!!!!!!!!', json_data)
         final = {'nodes': {}, 'edges': {}}
+
+        oid = ''
+        if json_data['submit_type'] == 'shortest_path_dst':
+            oid = self.name_to_sid(json_data['dst'])
+            print('OBGJECTID!!!', oid)
+        # elif json_data['submit_type'] ==
+        else:
+            return final
 
         nodes = []
         edges = []
         ncache = []
         ecache = []
 
-        testsid = 'S-1-5-21-3937601378-3721788405-2067139823-512'
+        # oid = 'S-1-5-21-3937601378-3721788405-2067139823-512'
         # nodes, edges, ncache, ecache = self.pathto(testsid, nodes, edges, ncache, ecache)
-        nodes, edges, ncache, ecache = self.add_node([testsid], nodes, edges, ncache, ecache)
+        nodes, edges, ncache, ecache = self.add_node([oid], nodes, edges, ncache, ecache)
 
         idx = 0
         for node in nodes:
@@ -43,55 +70,6 @@ class BloodRoostrWebApp:
             idx = idx + 1
 
         return json.dumps(final)
-
-    def pathto(self, sid, nodes, edges, ncache, ecache, depth=0, max_depth=5):
-        """ Find paths to target sid (recursively) """
-        start = self.db_session.query(EdgeLookup).filter_by(oid=sid).first()  # get id and typefrom sid
-        if start:
-            if start.oid not in ncache:
-                ncache.append(start.oid)
-                if start.otype == 'group':
-                    new_node = self.db_session.query(Group).filter_by(objectSid=start.oid).first()
-                    nodes.append(make_node(new_node.objectSid, new_node.name, 'group'))
-                elif start.otype == 'user':
-                    new_node = self.db_session.query(ADUser).filter_by(objectSid=start.oid).first()
-                    nodes.append(make_node(new_node.objectSid, new_node.name, 'user'))
-                elif start.otype == 'machine':
-                    new_node = self.db_session.query(Machine).filter_by(objectSid=start.oid).first()
-                    nodes.append(make_node(new_node.objectSid, new_node.name, 'computer'))
-
-        if depth == max_depth:
-            return nodes, edges, ncache, ecache
-        # final = {'nodes': {}, 'edges': {}}
-        # test = '{"nodes":{"1":{"id":"test1", "name":"FACK", "type":"computer"},"2":{"id":"test2", "name":"test2name", "type":"user"},"3":{"id":"test3", "name":"test3name", "type":"group"}},"edges":{"1":{"id":1, "from":"test1","to":"test2"},"2":{"id":2, "from":"test2","to":"test3"},"3":{"id":3, "from":"test3","to":"test1"}}}'
-        # da = 'S-1-5-21-3937601378-3721788405-2067139823-512'
-
-        # res = self.db_session.query(EdgeLookup).filter_by(oid=sid).first()  # get id from sid
-        res2 = self.db_session.query(Edge).filter_by(dst=start.id)  # get edges from id
-        idx = 0
-
-        for e in res2:  # for all edges
-            res = self.db_session.query(EdgeLookup).filter_by(id=e.src).first()  # get oid and otype from edge src
-
-            # encache = []
-            test = False
-            # for i in ncache:
-            #    if (res.oid, i) in ecache:
-            #        test = True
-            #        print('TESTING', i)
-            #        break
-                # encache.append(i[0])
-                # encache.append(i[1])
-
-            if (res.oid, start.oid) not in ecache and res.oid != start.oid:  # and res.oid not in encache:
-                ecache.append((res.oid, start.oid))
-                edges.append(make_edge(str(idx), res.oid, start.oid, e.label))
-                idx = idx + 1
-                nodes, edges, ncache, ecache = self.pathto(res.oid, nodes, edges, ncache, ecache, depth+1)
-            else:
-                print('not adding edge', res.oid, start.oid, e.label)
-
-        return nodes, edges, ncache, ecache
 
     def add_node(self, sids, nodes, edges, ncache, ecache):
         id_sid_map = {}
@@ -110,6 +88,14 @@ class BloodRoostrWebApp:
                     elif start.otype == 'machine':
                         new_node = self.db_session.query(Machine).filter_by(objectSid=start.oid).first()
                         nodes.append(make_node(new_node.objectSid, new_node.name, 'computer'))
+                    elif start.otype == 'gpo':
+                        new_node = self.db_session.query(GPO).filter_by(objectGUID=start.oid).first()
+                        nodes.append(make_node(new_node.objectGUID, new_node.name, 'gpo'))
+                    elif start.otype == 'ou':
+                        new_node = self.db_session.query(ADOU).filter_by(objectGUID=start.oid).first()
+                        nodes.append(make_node(new_node.objectGUID, new_node.name, 'ou'))
+                    else:
+                        print('UNKNOWN!@!!!!!!!')
                     id_sid_map[start.id] = start.oid
 
         for node_id, node_sid in id_sid_map.items():
